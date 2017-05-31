3c831a0af49d502b871e1325632c76ecb5d29b82
VladListing <chvch@mail.ru>
2017-05-31 16:04:14 +0700

    31-05-2017 добавление генератора

---
+++ Conversion from binary to csv/Generator  binary files/Program.cs
@@ -0,0 +1,106 @@
+﻿using Generator__binary_files;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generator__binary_files
+{
+    //описание структуры 'TradeRecord' 
+    #region 'TradeRecord'
+
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]// размещение в неуправляемый код
+    public struct TradeRecord
+    {
+
+        public int id;
+        public int account;
+        public double volume;
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 64)] //маршалинг в неуправляемый код
+        public string comment;
+
+
+        //конструктор "TradeRecord"
+        public TradeRecord(int a, int b, double c, string d)
+        {
+            id = a;
+            account = b;
+            volume = c;
+            comment = d;
+        }
+
+    }
+    #endregion
+
+
+    class Program
+    {
+        static void Main(string[] args) // точка входа
+        {
+            Console.WriteLine("");
+            Console.WriteLine("Введите количество строк для создаваемого бинарного файла  'D:\\Trade.dat' : ");
+            Console.WriteLine("");
+
+            string r = Console.ReadLine();
+            int quantityTradeRecodLine = Convert.ToInt32(r);//количество элементов массива  содержашего структуры типа TradeRecod----х
+            string path = @"D:\\Trade.dat";  //путь и имя будующего бинарного файла содержащего  структуры
+            int schetchik = 0;//счетчик
+
+            RandomString rnd = new RandomString();
+
+            //создание экземпляра структуры и инициализфция полей (присвоение значений)
+            #region 
+
+            TradeRecord[] trade = new TradeRecord[quantityTradeRecodLine]; // создание экземпяра структуры "TradeRecord" на X строк
+
+            for (int i = 0; i < quantityTradeRecodLine; i++)
+            {
+                trade[i] = new TradeRecord(0 + i, 777, 640 + i, rnd.ArrayRand());
+            }
+            #endregion
+
+
+            //секция критичная в части исключений
+            try
+            {
+                //создание экземпляра BinaryWriter (запись  в бинарный файл)
+                #region BinaryWriter
+                using (BinaryWriter writer = new BinaryWriter(File.Open(path, FileMode.OpenOrCreate)))// открывает поток для записи структур в файл
+                {
+
+                    #region
+                    foreach (TradeRecord t in trade)
+                    {
+                        writer.Write(t.id);
+                        writer.Write(t.account);
+                        writer.Write(t.volume);
+                        writer.Write(t.comment);
+
+                        schetchik = schetchik + 1;
+                    }
+                    Console.WriteLine("");
+                    Console.WriteLine("в бинарный файл 'D:\\Trade.dat' записано   {0}  строк(и) структуры 'trade'", schetchik);
+
+                    schetchik = 0;
+
+                    #endregion
+
+                }
+                #endregion
+
+            }
+
+            //вывод сообщения о возновении исключения
+            #region исключения
+            catch (Exception m)
+            {
+                Console.WriteLine(m.Message);
+            }
+            Console.ReadLine();
+            #endregion
+        }
+    }
+}
---
+++ Conversion from binary to csv/Generator  binary files/RandomString.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generator__binary_files
+{
+    class RandomString
+    {
+
+        // заполняем столбец коментариев случайными значениями
+        Random r = new Random();
+
+        string sumstroka = "";
+        int s = 0;
+        int z = 0;
+        int t = 0;
+        public string ArrayRand()
+        {
+            s = r.Next(0, 6);
+            t = r.Next(-1000, 0);
+            z = r.Next(0, 10000);
+
+            if (s == 0)
+            { sumstroka = "   trade:Sell   result:Profit     " + "   +" + z + " $"; }
+            else if (s == 1)
+            { sumstroka = "   trade:Sell   result:Loss           " + t + " $"; }
+            else if (s == 2)
+            { sumstroka = "   trade:Bay    result:Profit     " + "  +" + z + " $"; }
+            else if (s == 3)
+            { sumstroka = "   trade:Bay    result:Loss          " + t + " $"; }
+            else if (s == 4)
+            { sumstroka = "   trade:Bay    result:StopLoss  " + t + " $"; }
+            else if (s == 5)
+            { sumstroka = "   trade:Sell   result:Stoploss  " + t + " $"; }
+
+            else
+            { sumstroka = " - "; }
+
+            return sumstroka;
+        }
+   }
+}
